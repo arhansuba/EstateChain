a11b787fdd5540405a91600ac9e95fdf686ee2e9
Emirhan-Cavusoglu-sftw <emirhancavusoglu.sftw@gmail.com>
2024-10-13 12:48:34 +0100

    update readm

--- my-app/components/Navbar.tsx
+++ my-app/components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar: React.FC = () => {
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex justify-between items-center">
         <Link href="/" className="text-white text-xl font-bold">
-          Stellar Estate
+          EstateChain
         </Link>
 
         <div className="flex space-x-4">
